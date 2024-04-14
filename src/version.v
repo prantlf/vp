a11c22a5182e1374d30dd40e9dc6cf938a927c8a
Ferdinand Prantl <prantlf@gmail.com>
2024-04-15 01:08:00 +0200

    fix: Work around with --bump-major-0 instead of -0

--- src/version.v
+++ src/version.v
@@ -36,7 +36,7 @@ fn create_version(version string, commit bool, tag bool, opts &Opts) !(string, s
 			extra_args += ' --pre-id "${opts.pre_id}"'
 		}
 		if opts.bump_major_0 {
-			extra_args += ' -0'
+			extra_args += ' --bump-major-0'
 		}
 		if opts.nc_args.len > 0 {
 			extra_args += ' ${opts.nc_args}'
